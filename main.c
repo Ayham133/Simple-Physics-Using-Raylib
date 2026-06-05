ec713d814763605e884c955c96e78a698b475000
Ayham133 <ayhamatallah52@gmail.com>
2026-06-05 17:56:13 +0300

    added more objects to the scene, still no collision between them

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "raylib.h"
-#include "object.h"
+#include "include/object.h"
 #include <stdlib.h>
 #include <math.h>
 
@@ -15,14 +15,55 @@ int main(void) {
 
 	const float GRAVITATIONAL_ACCELERATION= 980.0f; // Pixiles
 
-	Object ball = {
-		.position = {window_height/2, window_width/2},
-		.Velocity = {0,0},
-		.Force = {0,0},
+	const int OBJECT_COUNT = 6;
+
+	Object *objects = malloc(OBJECT_COUNT * (sizeof *objects));
+	objects[0] = (Object){
+		.position = {window_width/2, window_height/2},
+		.Force = {0.0f, 0.0f},
+		.mass = 10.0f,
 		.raduis = 50,
-		.mass = 10.0f
+		.Velocity = {-100.0f, 0.0f}
 	};
 
+	objects[1] = (Object){
+		.position = {100, 200},
+		.Force = {0.0f, 0.0f},
+		.mass = 10.0f,
+		.raduis = 50,
+		.Velocity = {-180.0f, 0.0f}
+	};
+
+	objects[2] = (Object){
+		.position = {200, 50},
+		.Force = {0.0f, 0.0f},
+		.mass = 10.0f,
+		.raduis = 50,
+		.Velocity = {70.0f, 0.0f}
+	};
+	objects[3] = (Object){
+		.position = {300, 100},
+		.Force = {0.0f, 0.0f},
+		.mass = 10.0f,
+		.raduis = 50,
+		.Velocity = {-120.0f, 0.0f}
+	};
+	objects[4] = (Object){
+		.position = {500,550}, 
+		.Force = {0.0f, 0.0f},
+		.mass = 10.0f,
+		.raduis = 50,
+		.Velocity = {50.0f, 0.0f}
+	};
+	objects[5] = (Object){
+		.position = {700, 450},
+		.Force = {0.0f, 0.0f},
+		.mass = 10.0f,
+		.raduis = 50,
+		.Velocity = {200.0f, 0.0f}
+	};
+
+	int draggingIndex =-1; 
 	Vector2  prevMousePos = GetMousePosition();
 	Vector2 mouseVelocity = {0.0f, 0.0f};
 	bool dragging = false;
@@ -35,15 +76,12 @@ int main(void) {
 		// Get current mouse position
 		Vector2 currentMousePos = GetMousePosition();
 
+		
 		//---- Physics. ----
 		float dt = GetFrameTime();
-		float const Weight = ball.mass * GRAVITATIONAL_ACCELERATION;
-		float const Normal = Weight;
 		float const Mue = 5.0f;
 		float restitution = 0.8f;
 
-		// calculating the net applayed forces each farme and store them in the ball.Force
-		Vector2 ApplayedForces = (Vector2){0.0f, 0.0f};
 
 		// Calculate mouse velocity
 		if(dt > 0.0f){
@@ -52,152 +90,182 @@ int main(void) {
 		}
 
 		prevMousePos = currentMousePos;
-
-		// Make the ball followes the Cursor when Collision happen
-		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && CheckCollisionPointCircle(ball.position, GetMousePosition(), 50.0f)){
-			dragging = true;
-			ball.position = GetMousePosition();
-			ball.Force = (Vector2){0,0};
-			ball.Velocity = (Vector2){0,0};
-		}
-
-		if(IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
-			dragging = false;
-			ball.Velocity = mouseVelocity;
-		}
-
-		/**
-		 *
-		 * 
-		 * Ball physics
-		 *
-		 */
-
-		 /**
-		  * 
-		  * Y-axis Forces and Collisions
-		  *
-		  * v = v + a *dt 
-		  *  
-		  */
-
-		 // When the ball is in free fall.
-		if (!CheckCollisionWithGround(ball.position.y, ball.raduis, window_height) && !dragging)
+		// Handle dragging logic outside the loop to ensure only one object is dragged at a time
+		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && draggingIndex == -1)
 		{
-			ApplayedForces.y += Weight;
-			ball.Force.y += ApplayedForces.y;
-
-			ball.Velocity.y += GRAVITATIONAL_ACCELERATION * dt;
-			ball.position.y += ball.Velocity.y * dt;
-		}
-
-		// When the ball hits the ground the forces in the y-axis is zero thus the velocity should be zero
-		if(CheckCollisionWithGround(ball.position.y, ball.raduis, window_height) && !dragging){
-			ball.Force.y = 0;
-
-			if(fabs(ball.Velocity.y) < 0.9f){
-				ball.Velocity.y = 0.0f;
-				ball.position.y = window_height - ball.raduis;
-			}
-			else
+			for (int i = 0; i < OBJECT_COUNT; i++)
 			{
-				ball.Velocity.y *= -restitution;
-				ball.position.y += ball.Velocity.y *dt;
-			}
-
-		}
-
-
-		/**
-		 * 
-		 * X-axis Forces and Collision  
-		 *
-		 * updating the x postion is on the line  183
-		 */
-
-		// When in free fall the horizontal forces should be zero.
-		if(!CheckCollisionWithGround(ball.position.y, ball.raduis, window_height) && !dragging)
-		{
-			ball.Force.x = 0;
-		}
-		// When the ball on the ground add friction. 
-		if(CheckCollisionWithGround(ball.position.y, ball.raduis, window_height) && !dragging)
-		{
-			// friction force
-			float FrictionForce = 30.0f;
-			// update the forces on the ball.
-			ball.Force.x = Mue * Weight;
-			// calculate the magnatued of the ball.Velocity vector.
-			float Velocity = sqrtf((ball.Velocity.x * ball.Velocity.x) + (ball.Velocity.y * ball.Velocity.y));
-
-			// apply friction just when the ball is moveing along the x-axis
-			if(Velocity > 0.0f){
-				float speedReduction = FrictionForce * dt;
-
-				// stop friction when the speed smaller than the force of friction (just stop the ball bro).
-				if(Velocity <= speedReduction){
-					ball.Velocity.x = 0.0f;
-					ball.Force.x = 0;
-				}
-				// else apply the friction calculated.
-				else{
-					ball.Velocity.x -= (ball.Velocity.x / Velocity) * speedReduction;
+				if (CheckCollisionPointCircle(objects[i].position, GetMousePosition(), 50.0f))
+				{
+					draggingIndex = i;
+					dragging = true;
+					objects[i].position = GetMousePosition();
+					objects[i].Force = (Vector2){0, 0};
+					objects[i].Velocity = (Vector2){0, 0};
+					break;
 				}
 			}
 		}
-
-		// Right wall Collision and Bouncing
-		if (CheckCollisionWithRightWindowEdge(ball.position.x, ball.raduis, window_width, ball.Velocity.x) && !dragging)
+		else if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && draggingIndex != -1)
 		{
-			// Correct penetration, snapping the ball back by (window_width - ball.raduis)/ the exact position where the ball touches the wall.
-			ball.position.x = window_width - ball.raduis;
-			// Bounce, by the equation v' = -e * v
-			ball.Velocity.x *= -restitution;
+			objects[draggingIndex].position = GetMousePosition();
+			objects[draggingIndex].Force = (Vector2){0, 0};
+			objects[draggingIndex].Velocity = (Vector2){0, 0};
 		}
 
-		// Left wall Collision and Bouncing
-		if (CheckCollisionWithLeftWindowEdge(ball.position.x, ball.raduis, ball.Velocity.x) && !dragging)
+		if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT) && draggingIndex != -1)
 		{
-			// Correct penetration, snapping the ball back by (ball.raduis)/ the exact position where the ball touches the wall.
-			ball.position.x = ball.raduis;
-			// Bounce, by the equation v' = -e * v
-			ball.Velocity.x *= -restitution;
+			objects[draggingIndex].Velocity = mouseVelocity;
+			draggingIndex = -1;
+			dragging = false;
 		}
 
-		// updating the x position of the ball after all the calculation of the ball.Velocity.x
-		ball.position.x += ball.Velocity.x * dt;
+		for (int i = 0; i < OBJECT_COUNT; i++)
+		{
+			float const Weight = objects[i].mass * GRAVITATIONAL_ACCELERATION;
+			// calculating the net applayed forces each farme and store them in the objects[i].Force
+			Vector2 ApplayedForces = (Vector2){0.0f, 0.0f};
 
-		// Reset the ballPositionY
-		if (IsKeyDown(KEY_SPACE)) {
-			ball.position.y = 100;
-			ball.position.x = window_width/2;
-			ball.Force = (Vector2){.x = 0.0f, .y = 0.0f};
-			ball.Velocity = (Vector2){.x = 0.0f, .y = 0.0f};
+
+			/**
+			 *
+			 *
+			 * objects[i].physics
+			 *
+			 */
+
+			/**
+			 *
+			 * Y-axis Forces and Collisions
+			 *
+			 * v = v + a *dt
+			 *
+			 */
+
+			// When the objects[i].is in free fall.
+			// Only skip physics for the object currently being dragged.
+			if (!CheckCollisionWithGround(objects[i].position.y, objects[i].raduis, window_height) && i != draggingIndex)
+			{
+				ApplayedForces.y += Weight;
+				objects[i].Force.y += ApplayedForces.y;
+
+				objects[i].Velocity.y += GRAVITATIONAL_ACCELERATION * dt;
+				objects[i].position.y += objects[i].Velocity.y * dt;
+			}
+
+			// When the objects[i].hits the ground the forces in the y-axis is zero thus the velocity should be zero
+			if (CheckCollisionWithGround(objects[i].position.y, objects[i].raduis, window_height) && i != draggingIndex)
+			{
+				objects[i].Force.y = 0;
+
+				if (fabs(objects[i].Velocity.y) < 0.9f)
+				{
+					objects[i].Velocity.y = 0.0f;
+					objects[i].position.y = window_height - objects[i].raduis;
+				}
+				else
+				{
+					objects[i].Velocity.y *= -restitution;
+					objects[i].position.y += objects[i].Velocity.y * dt;
+				}
+			}
+
+			/**
+			 *
+			 * X-axis Forces and Collision
+			 *
+			 * updating the x postion is on the line  183
+			 */
+
+			// When in free fall the horizontal forces should be zero.
+			if (!CheckCollisionWithGround(objects[i].position.y, objects[i].raduis, window_height) && i != draggingIndex)
+			{
+				objects[i].Force.x = 0;
+			}
+			// When the objects[i].on the ground add friction.
+			if (CheckCollisionWithGround(objects[i].position.y, objects[i].raduis, window_height) && i != draggingIndex)
+			{
+				// friction force
+				float FrictionForce = 30.0f;
+				// update the forces on the objects[i].
+				objects[i].Force.x = Mue * Weight;
+				// calculate the magnatued of the objects[i].Velocity vector.
+				float Velocity = sqrtf((objects[i].Velocity.x * objects[i].Velocity.x) + (objects[i].Velocity.y * objects[i].Velocity.y));
+
+				// apply friction just when the objects[i].is moveing along the x-axis
+				if (Velocity > 0.0f)
+				{
+					float speedReduction = FrictionForce * dt;
+
+					// stop friction when the speed smaller than the force of friction (just stop the objects[i].bro).
+					if (Velocity <= speedReduction)
+					{
+						objects[i].Velocity.x = 0.0f;
+						objects[i].Force.x = 0;
+					}
+					// else apply the friction calculated.
+					else
+					{
+						objects[i].Velocity.x -= (objects[i].Velocity.x / Velocity) * speedReduction;
+					}
+				}
+			}
+
+			// Right wall Collision and Bouncing
+			if (CheckCollisionWithRightWindowEdge(objects[i].position.x, objects[i].raduis, window_width, objects[i].Velocity.x) && i != draggingIndex)
+			{
+				// Correct penetration, snapping the objects[i].back by (window_width - ball.raduis)/ the exact position where the ball touches the wall.
+				objects[i].position.x = window_width - objects[i].raduis;
+				// Bounce, by the equation v' = -e * v
+				objects[i].Velocity.x *= -restitution;
+			}
+
+			// Left wall Collision and Bouncing
+			if (CheckCollisionWithLeftWindowEdge(objects[i].position.x, objects[i].raduis, objects[i].Velocity.x) && i != draggingIndex)
+			{
+				// Correct penetration, snapping the objects[i].back by (ball.raduis)/ the exact position where the ball touches the wall.
+				objects[i].position.x = objects[i].raduis;
+				// Bounce, by the equation v' = -e * v
+				objects[i].Velocity.x *= -restitution;
+			}
+
+			// updating the x position of the objects[i].after all the calculation of the ball.Velocity.x
+			objects[i].position.x += objects[i].Velocity.x * dt;
+
+			// Reset the objects[i].ositionY
+			if (IsKeyDown(KEY_SPACE))
+			{
+				objects[i].position.y = 100;
+				objects[i].position.x = window_width / 2;
+				objects[i].Force = (Vector2){.x = 0.0f, .y = 0.0f};
+				objects[i].Velocity = (Vector2){.x = 0.0f, .y = 0.0f};
+			}
+
+			
 		}
-
-
-
-		/**
-		 * 
-		 * Drawing and rendering.
-		 * 
-		 */
-		float ballVelocityX = ball.Velocity.x;
-		float ballVelocityY = ball.Velocity.y;
-
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
-		DrawText(TextFormat("Ball's Velocity {%.3f, %.3f}, FPS: %i",ballVelocityX, ballVelocityY, GetFPS()),10, 10, 20, BLACK);
-		DrawText(TextFormat("Mouse Velocity: {%.3f, %.3f}", mouseVelocity.x, mouseVelocity.y), 10, 40, 20, GRAY);
-		if(dragging)
-			DrawText("Dragging: enabled", 10, 60, 20, GREEN);
-		else
-			DrawText("Dragging: disabled", 10, 60, 20, RED);
+		for (int i = 0; i < OBJECT_COUNT; i++)
+		{
+			/**
+			 *
+			 * Drawing and rendering.
+			 *
+			 */
+			// float ballVelocityX = objects[i].Velocity.x;
+			// float ballVelocityY = objects[i].Velocity.y;
 
-		DrawText("Click space to reset the y position of the ball!.", 10, 80, 20, LIGHTGRAY);
+			// DrawText(TextFormat("objects[i].s Velocity {%.3f, %.3f}, FPS: %i", ballVelocityX, ballVelocityY, GetFPS()), 10, 10, 20, BLACK);
+			DrawText(TextFormat("Mouse Velocity: {%.3f, %.3f}", mouseVelocity.x, mouseVelocity.y), 10, 40, 20, GRAY);
+			if (dragging)
+				DrawText("Dragging: enabled", 10, 60, 20, GREEN);
+			else
+				DrawText("Dragging: disabled", 10, 60, 20, RED);
 
-		DrawCircleV(ball.position, ball.raduis, RED);
+			DrawText("Click space to reset the y position of the objects[i]..", 10, 80, 20, LIGHTGRAY);
 
+			DrawCircleV(objects[i].position, objects[i].raduis, RED);
+		}
 		EndDrawing();
 	}
 
